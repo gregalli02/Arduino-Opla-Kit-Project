d2d3e9a4b8f3e7ace4f58acdf9d3d3d1d9192f2a
Alessandro Pischedda <alessandro.pischedda@gmail.com>
2021-08-12 10:55:55 +0200

    Moved configuration variables and messages into separated file colled "config.c".

---
+++ config.c
@@ -0,0 +1,16 @@
+/* This file contains the configuration and the messages too */
+
+// settings
+char ssid[]        = "SSID of your WiFi";
+char pass[]        = "Password of your WiFi";
+char auth[]        = "Auth code of Blynk";
+bool serialEnabled = false; // set it true to see debug messages on serial
+
+// messages
+char welcomeMessage[]   = "Hi Greta :)";
+char intruderMsg[]      = "Intruder";
+char alarmEnabledMsg[]  = "Alarm Enabled";
+char alarmDisabledMsg[] = "Alarm Disabled";
+char alarmActionMsg1[]  = "Press 2 to enable";
+char alarmActionMsg2[]  = "or disable alarm";
+
